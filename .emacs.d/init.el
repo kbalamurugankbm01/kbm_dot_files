575569044aad698f3de3edc509599420af3b42e2
Kangayam-Bull <kangayambullmachineries@gmail.com>
2024-11-28 23:15:11 +0530

    Resetting the emacs - init.el with fresh start

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,50 +1,170 @@
-;;*****************************************************************
-;;
-;;                     KBM's emacs config - Init
-;;
-;;*****************************************************************
+;;; init.el --- Emfy 0.4.0 <https://github.com/susam/emfy>
 
-;;BuiltIn Package System
-(require 'package)
-(add-to-list 'package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+;;; Commentary:
 
-(package-initialize)
+;; A very basic dark and sleek Emacs setup for general purpose
+;; editing and programming.
 
-;;-----------------------------------------------------------------
-;;                         User Interface
-;;-----------------------------------------------------------------
-
-;;(tool-bar-mode -1)     ; Disable toolbar
-;;(menu-bar-mode -1)     ; Disable the menubar
-;;(tooltip-mode -1)      ; Disable tooltips
-
-;;(scroll-bar-mode -1)   ; Disable visible scrollbar
-(set-fringe-mode -1)   ; Give some breathing room
-
-(global-display-line-numbers-mode 1)   ; Enable line numbers on left margin
-
-;;Setup the visible bell instead of sound queue
-(setq visible-bell t)
+;;; Code:
 
 
-;;Setting Themes
-(load-theme 'modus-operandi)
+;;; Look and Feel ====================================================
 
-(set-face-attribute 'default nil :font "Intel One Mono" :height 100)
-;;----------------------------------------------------------------
-;; Functionalities
-;;----------------------------------------------------------------
+;; Tweak UI.
+(when (display-graphic-p)
+  (tool-bar-mode 0)
+  (scroll-bar-mode 0))
+(setq inhibit-startup-screen t)
+(column-number-mode)
 
-;;Activating the EVIL mode
-;;(require 'evil)
-;;(evil-mode 1)
+;; Dark theme.
+(load-theme 'gruvbox)
+(with-eval-after-load 'gruvbox-theme
+  (set-face-background 'default "#111")
+  (set-face-background 'cursor "#c96")
+  (set-face-foreground 'font-lock-comment-face "#fc0")
+  (set-face-background 'isearch "#ff0")
+  (set-face-foreground 'isearch "#000")
+  (set-face-background 'lazy-highlight "#990")
+  (set-face-foreground 'lazy-highlight "#000"))
 
-;; Ledger-Mode
-  (use-package ledger-mode
-    :mode ("\\.dat\\'"
-           "\\.ledger\\'")
-    :custom (ledger-clear-whole-transactions t))
+;; Enable line numbers while writing config, code, or text.
+(dolist (hook '(prog-mode-hook conf-mode-hook text-mode-hook))
+  (add-hook hook 'display-line-numbers-mode))
 
-  (use-package flycheck-ledger :after ledger-mode)
+;; Highlight matching pairs of parentheses.
+(setq show-paren-delay 0)
+(show-paren-mode)
+
+;; Auto-complete inputs in the minibuffer.
+(fido-vertical-mode)
+
+;; (set-face-attribute 'default nil :font "Intel One Mono" :height 144)
+;; (set-face-attribute 'default nil :font "Fira Code" :height 136)
+;; (set-face-attribute 'default nil :font "JetBrains Mono" :height 136)
+;; (set-face-attribute 'default nil :font "IBM Plex Mono" :height 136)
+;; (set-face-attribute 'default nil :font "Consolas" :height 136)
+(set-face-attribute 'default nil :font "Roboto Mono" :height 136)
+
+;;; Whitespace =======================================================
+
+;; Show trailing whitespace while writing config, code, or text.
+(dolist (hook '(conf-mode-hook prog-mode-hook text-mode-hook))
+  (add-hook hook (lambda () (setq show-trailing-whitespace t))))
+
+;; Show stray blank lines.
+(setq-default indicate-empty-lines t)
+(setq-default indicate-buffer-boundaries 'left)
+
+;; Add a newline automatically at the end of a file while saving.
+(setq require-final-newline t)
+
+;; Consider a period followed by a single space to be end of sentence.
+(setq sentence-end-double-space nil)
+
+;; Use spaces, not tabs, for indentation.
+(setq-default indent-tabs-mode nil)
+
+;; Display the distance between two tab stops as 4 characters wide.
+(setq-default tab-width 4)
+
+;; Indentation setting for various languages.
+(setq c-basic-offset 4)
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+
+
+;;; Clean Working Directories ========================================
+
+;; Write auto-saves and backups to separate directory.
+(make-directory "~/.tmp/emacs/auto-save/" t)
+(setq auto-save-file-name-transforms '((".*" "~/.tmp/emacs/auto-save/" t)))
+(setq backup-directory-alist '(("." . "~/.tmp/emacs/backup/")))
+
+;; Do not move the current file while creating backup.
+(setq backup-by-copying t)
+
+;; Disable lockfiles.
+(setq create-lockfiles nil)
+
+;; Write customizations to a separate file instead of this file.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file t)
+
+
+;;; Custom Command and Key Sequences =================================
+
+;; Custom command.
+(defun show-current-time ()
+  "Show current time."
+  (interactive)
+  (message (current-time-string)))
+
+;; Custom key sequences.
+(global-set-key (kbd "C-c t") 'show-current-time)
+(global-set-key (kbd "C-c d") 'delete-trailing-whitespace)
+
+
+;;; Emacs Server =====================================================
+
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+
+;;; Package Setup ====================================================
+
+(defun install-packages ()
+  "Install and set up packages for the first time."
+  (interactive)
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-refresh-contents)
+  (dolist (package '(markdown-mode paredit rainbow-delimiters))
+    (unless (package-installed-p package)
+      (package-install package))))
+
+
+;;; Paredit Configuration ============================================
+
+;; Enable Paredit on various Lisp modes.
+(when (fboundp 'paredit-mode)
+  (dolist (hook '(emacs-lisp-mode-hook
+                  eval-expression-minibuffer-setup-hook
+                  ielm-mode-hook
+                  lisp-interaction-mode-hook
+                  lisp-mode-hook))
+    (add-hook hook 'enable-paredit-mode)))
+
+;; Do not bind RET to paredit-RET which prevents input from being
+;; evaluated on RET in M-:, ielm, etc.
+(with-eval-after-load 'paredit
+  (define-key paredit-mode-map (kbd "RET") nil))
+
+
+;;; Rainbow Delimiters Configuration =================================
+
+(when (fboundp 'rainbow-delimiters-mode)
+  (dolist (hook '(emacs-lisp-mode-hook
+                  ielm-mode-hook
+                  lisp-interaction-mode-hook
+                  lisp-mode-hook))
+    (add-hook hook 'rainbow-delimiters-mode)))
+
+(with-eval-after-load 'rainbow-delimiters
+  (set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
+  (set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
+  (set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
+  (set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
+  (set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
+  (set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
+  (set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
+  (set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
+  (set-face-foreground 'rainbow-delimiters-depth-9-face "#666")) ; dark gray
+
+
+;;; The End ==========================================================
+
+(provide 'init)
+
+;;; init.el ends here
